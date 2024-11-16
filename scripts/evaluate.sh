676a5f97eb52342920ae2c8d29999af331768774
fpy <fupeiyi@foxmail.com>
2024-11-16 16:35:40 +0800

    test: allow specifying case from cmd options

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -2,9 +2,15 @@
 # 需在build/bin/目录下运行  
 # 使用-t参数指定测试数据路径 使用-s参数指定输出的design.fpga.out文件路径  
 # 其功能是评估design.fpga.out结果是否合法  
-../../etc/evaluator \
-  -t ../../data/case03 \
-  -s ./design.fpga.out \
+if [ ! -z "$1" ]; then
+  ../../etc/evaluator \
+    -t ../../data/case0$1 \
+    -s ./design.fpga.out
+else
+  ../../etc/evaluator \
+    -t ../../data/case02 \
+    -s ./design.fpga.out
+fi
 
 # 赛题服务器评估命令
 # /home/public/evaluator/evaluator \
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,15 @@
 # 需在build/bin/目录下运行  
 # 使用-t参数指定测试数据路径 使用-s参数指定输出路径  
 # 会在build/bin/目录下生成结果文件design.fpga.out  
-./partitioner \
-  -t ../../data/case01 \
-  -s ./design.fpga.out \
+if [ ! -z "$1" ]; then
+  ./partitioner \
+    -t ../../data/case0$1 \
+    -s ./design.fpga.out
+else
+  ./partitioner \
+    -t ../../data/case02 \
+    -s ./design.fpga.out
+fi
 
 # 赛题服务器运行命令
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/eda240327_submission/submission/bin
