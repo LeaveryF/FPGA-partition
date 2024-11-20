4a127899b30244e35c7b0c2a4988a3cab7982648
fpy <fupeiyi@foxmail.com>
2024-11-20 17:50:55 +0800

    test: bug about mt

--- scripts/mt_run.sh
+++ scripts/mt_run.sh
@@ -1,5 +1,6 @@
 # 运行可执行文件bin/MtKaHyPar  
 # 需在build/bin/目录下运行  
+# 根据需要修改参数
 ../../bin/MtKaHyPar \
   -h ../../examples/ibm01.hgr \
   --preset-type=deterministic \
@@ -8,6 +9,5 @@
   -e 0.03 \
   -g ../../examples/target.graph \
   -o steiner_tree \
-  --write-partition-file=true \
-  --partition-output-folder=. \
+  # -g ./instances/graph_no_newline.graph \
   # -o km1 \
